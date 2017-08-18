1b43783b3850e23dd0f7d80b18f18f8be67b615f
Tomer Gev <tomergev21@gmail.com>
2017-08-18 12:12:45 -0700

    Further Editting

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var app = express();
-var Client = require('node-rest-client').Client
-
-var client = new Client()
 
 app.listen(3030, function () {
     console.log('Magic Happening on Port 3030')
@@ -11,12 +8,6 @@ app.listen(3030, function () {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-
-
-client.get('https://api.agent.com/public/profiles', { headers: { 'X-Authorization': '}kpJ/}RUXG7*6qm2wM6^ZX=x3?7+&TM=' } }, function (response) {
-    module.exports = response 
-})
-
 
 
 
